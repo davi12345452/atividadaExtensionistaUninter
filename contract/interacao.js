2314bdd8e6b7b238bd7ffc991f9ba10d5fb39925
DaviMaia <davijanischmaia@gmail.com>
2023-08-14 21:11:45 -0300

    ajustes

--- contract/interacao.js
+++ contract/interacao.js
@@ -2,52 +2,34 @@ const ethers = require('ethers')
 require('dotenv').config()
 const ABI = require('./contractABI.json')
 
-// Links RPCs: https://www.alchemy.com/chain-connect/chain/mumbai
 
-// Permite pegar um nó para se comunicar com a Blockchain
 const provider = new ethers.providers.JsonRpcProvider(
   process.env.URL_RPC
 );
-  
-const pk = process.env.PRIVATE_KEY // Private Key do Master do contrato
-
-// Aqui, usando o Nó, o endereço do contrato e o ABI do código, criamos uma instância manipulável do contrato. Podemos
-// ler dados do contrato ou chamar funções que devem ser assinadas
+const pk = process.env.PRIVATE_KEY 
 const contractInstance = new ethers.Contract(
-  process.env.CONTRACT_ADDRESS, // Endereço do contrato proxy CLNT
+  process.env.CONTRACT_ADDRESS, 
   ABI,
   provider,
 );
-  
-// Cria uma wallet com private key
 const signer = new ethers.Wallet(pk);
-
-// Conecta a wallet com o nó da rede
 const connectedSigner = signer.connect(provider);
-
-// Cria uma instância associando o contrato específico com a private key, isso permite chamar funções Write e assiná-las
-// Esse contract serve para chamar funções pagas, que modificam a blockchain, se for somente leitura, usa o contractInstance
 const contract = contractInstance.connect(connectedSigner);
 
-// Se der underpriced ou overpriced, mexe aqui que resolve
+
 const gasPrice = ethers.BigNumber.from('10000000000');
 const gasLimit = ethers.BigNumber.from('300000');
 
-const convertNumber = value => ethers.utils.parseUnits(value.toString(), "18");
-
-// Verificar tokens CNLT duas carteiras
 
 async function claimData(id){
-  const _data = await contract.getArq(id.toString())
-  return [_data.hash, Date(Number(_data.data))]
+  const dados = await contract.getArq(id.toString())
+  return [dados.hashText, Date(Number(dados.data))]
 }
 
-async function claimUri(id){
-  const uri = await contract.tokenURI(id.toString())
-  return uri
+async function claimUri(id) {
+  return await contract.tokenURI(id.toString())
 }
 
-// Chama a função TransferFrom assinando com a private key aqui do código
 
 async function mint(hash, uri) {
 
@@ -66,6 +48,6 @@ async function mint(hash, uri) {
   console.log(`transacao confirmada: ${tx.hash}`);
 }
 
-//mint("rufgrweiufghewrigherguure", "https://www.google.json")
-//claimData(0)
-//claimUri(0)
+module.exports = {mint, claimUri, claimData}
+
+
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const integration = require('./contract/interacao')
 
 const express = require("express");
 const app = express();
@@ -13,13 +14,18 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Sucess')
+    res.send("Sucess")
 })
 
 // Rota do metadado
 app.get('/metadatas/:id', (req, res) => {
     const id = req.params.id;
-    res.send(id)
+    integration.claimData(id).then(data => {
+        res.send(`
+            Hash do arquivo NFT id ${id} : ${data[0]}
+            Data de criação: ${data[1]}
+        `)
+    }).catch(e => console.log(e))
 })
 
 
