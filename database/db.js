9c5ba9ce93ca2a2cb88b5b9830315457e899ee66
DaviMaia <davijanischmaia@gmail.com>
2023-08-23 20:55:01 -0300

    Ajeitando

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize(process.env.NOME_DO_DB, process.env.USUARIO_MYSQL, process.env.SENHA_MYSQL, {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
-});
-
-module.exports = connection;
